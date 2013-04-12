51f1df1b6eb7a1e539d5064d694a1b7a2fecb3fe
William Trevor Olson <wtolson@gmail.com>
2013-04-11 18:56:19 -0700

    Update errors.

--- errors.py
+++ errors.py
@@ -19,6 +19,10 @@ class NSQInvalid(NSQException):
     """E_INVALID"""
     pass
 
+class NSQBadBody(NSQException):
+    """E_BAD_BODY"""
+    pass
+
 class NSQBadTopic(NSQException):
     """E_BAD_TOPIC"""
     pass
@@ -35,22 +39,38 @@ class NSQPutFailed(NSQException):
     """E_PUT_FAILED"""
     pass
 
+class NSQPubFailed(NSQException):
+    """E_PUB_FAILED"""
+
+class NSQMPubFailed(NSQException):
+    """E_MPUB_FAILED"""
+
 class NSQFinishFailed(NSQException):
-    """E_FINISH_FAILED"""
+    """E_FIN_FAILED"""
     pass
 
 class NSQRequeueFailed(NSQException):
-    """E_REQUEUE_FAILED"""
+    """E_REQ_FAILED"""
+    pass
+
+class NSQTouchFailed(NSQException):
+    """E_TOUCH_FAILED"""
     pass
 
 ERROR_CODES = {
     'E_INVALID':        NSQInvalid,
+    'E_BAD_BODY':       NSQBadBody,
     'E_BAD_TOPIC':      NSQBadTopic,
     'E_BAD_CHANNEL':    NSQBadChannel,
     'E_BAD_MESSAGE':    NSQBadMessage,
     'E_PUT_FAILED':     NSQPutFailed,
+    'E_PUB_FAILED':     NSQPubFailed,
+    'E_MPUB_FAILED':    NSQMPubFailed,
     'E_FINISH_FAILED':  NSQFinishFailed,
-    'E_REQUEUE_FAILED': NSQRequeueFailed
+    'E_FIN_FAILED':     NSQFinishFailed,
+    'E_REQUEUE_FAILED': NSQRequeueFailed,
+    'E_REQ_FAILED':     NSQRequeueFailed,
+    'E_TOUCH_FAILED':   NSQTouchFailed
 }
 
 def make_error(error_code):
