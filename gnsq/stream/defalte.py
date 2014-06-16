d70ccd856bb4ddb061ff608716ef15f778380d62
William Trevor Olson <wtolson@gmail.com>
2014-06-16 16:19:08 -0700

    Set correct waits for deflate.

--- gnsq/stream/defalte.py
+++ gnsq/stream/defalte.py
@@ -6,8 +6,9 @@ from .compression import CompressionSocket
 
 class DefalteSocket(CompressionSocket):
     def __init__(self, socket, level):
-        self._decompressor = zlib.decompressobj(level)
-        self._compressor = zlib.compressobj(level)
+        wbits = -zlib.MAX_WBITS
+        self._decompressor = zlib.decompressobj(wbits)
+        self._compressor = zlib.compressobj(level, zlib.DEFLATED, wbits)
         super(DefalteSocket, self).__init__(socket)
 
     def compress(self, data):
