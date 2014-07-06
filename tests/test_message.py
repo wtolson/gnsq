35bd9d2de64ba0e86e62e7c969d2e5f2f05aa7fc
William Trevor Olson <wtolson@gmail.com>
2014-07-05 22:24:11 -0700

    Message tests.

---
+++ tests/test_message.py
@@ -0,0 +1,101 @@
+import pytest
+import gnsq
+
+
+class MockConnection(object):
+    def __init__(self, operations):
+        self.operations = iter(operations)
+
+    def __getattr__(self, name):
+        expected_name, expected_args = self.operations.next()
+        assert name == expected_name
+
+        def check_args(*args):
+            assert args == expected_args
+
+        return check_args
+
+    def assert_finished(self):
+        with pytest.raises(StopIteration):
+            self.operations.next()
+
+
+def test_basic():
+    message = gnsq.Message(None, 0, 42, '1234', 'sup')
+    assert message.id == '1234'
+    assert message.body == 'sup'
+    assert message.has_responded() is False
+
+
+def test_finish():
+    mock_conn = MockConnection([
+        ('finish', ('1234',)),
+    ])
+
+    message = gnsq.Message(mock_conn, 0, 42, '1234', 'sup')
+    assert message.has_responded() is False
+
+    message.finish()
+    assert message.has_responded() is True
+
+    with pytest.raises(gnsq.errors.NSQException):
+        message.finish()
+
+
+def test_requeue():
+    mock_conn = MockConnection([
+        ('requeue', ('1234', 0)),
+    ])
+
+    message = gnsq.Message(mock_conn, 0, 42, '1234', 'sup')
+    assert message.has_responded() is False
+
+    message.requeue()
+    assert message.has_responded() is True
+
+    with pytest.raises(gnsq.errors.NSQException):
+        message.requeue()
+
+    mock_conn.assert_finished()
+
+
+def test_requeue_timeout():
+    mock_conn = MockConnection([
+        ('requeue', ('1234', 1000)),
+    ])
+
+    message = gnsq.Message(mock_conn, 0, 42, '1234', 'sup')
+    assert message.has_responded() is False
+
+    message.requeue(1000)
+    assert message.has_responded() is True
+
+    with pytest.raises(gnsq.errors.NSQException):
+        message.requeue(1000)
+
+    mock_conn.assert_finished()
+
+
+def test_touch():
+    mock_conn = MockConnection([
+        ('touch', ('1234',)),
+        ('touch', ('1234',)),
+        ('touch', ('1234',)),
+        ('finish', ('1234',)),
+    ])
+
+    message = gnsq.Message(mock_conn, 0, 42, '1234', 'sup')
+    assert message.has_responded() is False
+
+    message.touch()
+    message.touch()
+    message.touch()
+    assert message.has_responded() is False
+
+    message.finish()
+    assert message.has_responded() is True
+
+    with pytest.raises(gnsq.errors.NSQException):
+        message.touch()
+
+    mock_conn.assert_finished()
