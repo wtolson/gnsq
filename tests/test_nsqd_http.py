ad71258d5657b69a8ace2a9af48b82a349ddfc90
William Trevor Olson <wtolson@gmail.com>
2014-07-05 20:50:12 -0700

    Add nsqd http tests.

---
+++ tests/test_nsqd_http.py
@@ -0,0 +1,74 @@
+from __future__ import with_statement
+
+import pytest
+import gnsq
+from integration_server import IntegrationNsqdServer
+
+
+@pytest.mark.slow
+def test_basic():
+    with IntegrationNsqdServer() as server:
+        conn = gnsq.Nsqd(server.address, http_port=server.http_port)
+        assert conn.ping() == 'OK'
+        assert 'topics' in conn.stats()
+        assert 'version' in conn.info()
+
+
+@pytest.mark.slow
+def test_topics_channels():
+    with IntegrationNsqdServer() as server:
+        conn = gnsq.Nsqd(server.address, http_port=server.http_port)
+        assert len(conn.stats()['topics']) == 0
+
+        with pytest.raises(gnsq.errors.NSQHttpError):
+            conn.delete_topic('topic')
+
+        conn.create_topic('topic')
+        topics = conn.stats()['topics']
+        assert len(topics) == 1
+        assert topics[0]['topic_name'] == 'topic'
+
+        conn.delete_topic('topic')
+        assert len(conn.stats()['topics']) == 0
+
+        with pytest.raises(gnsq.errors.NSQHttpError):
+            conn.create_channel('topic', 'channel')
+
+        with pytest.raises(gnsq.errors.NSQHttpError):
+            conn.delete_channel('topic', 'channel')
+
+        conn.create_topic('topic')
+        assert len(conn.stats()['topics'][0]['channels']) == 0
+
+        conn.create_channel('topic', 'channel')
+        channels = conn.stats()['topics'][0]['channels']
+        assert len(channels) == 1
+        assert channels[0]['channel_name'] == 'channel'
+
+        conn.delete_channel('topic', 'channel')
+        assert len(conn.stats()['topics'][0]['channels']) == 0
+
+
+def test_publish():
+    with IntegrationNsqdServer() as server:
+        conn = gnsq.Nsqd(server.address, http_port=server.http_port)
+
+        conn.publish_http('topic', 'sup')
+        assert conn.stats()['topics'][0]['depth'] == 1
+
+        conn.multipublish_http('topic', ['sup', 'sup'])
+        assert conn.stats()['topics'][0]['depth'] == 3
+
+        conn.multipublish_http('topic', iter(['sup', 'sup', 'sup']))
+        assert conn.stats()['topics'][0]['depth'] == 6
+
+        conn.empty_topic('topic')
+        assert conn.stats()['topics'][0]['depth'] == 0
+
+        conn.create_topic('topic')
+        conn.create_channel('topic', 'channel')
+        conn.publish_http('topic', 'sup')
+        assert conn.stats()['topics'][0]['channels'][0]['depth'] == 1
+
+        conn.empty_channel('topic', 'channel')
+        assert conn.stats()['topics'][0]['channels'][0]['depth'] == 0
