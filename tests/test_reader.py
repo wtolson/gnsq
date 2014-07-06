3ef5006ef9238ae264fff3479c5619794eca854f
William Trevor Olson <wtolson@gmail.com>
2014-07-06 01:10:16 -0700

    Test reader basic functions, max concurrency, lookupd and back off.

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,7 +1,57 @@
+from __future__ import with_statement
+
+import multiprocessing
 import pytest
-from gnsq import Nsqd, Reader
+import gevent
+
+from gnsq import Nsqd, Reader, states
 from gnsq.errors import NSQSocketError
-from integration_server import NsqdIntegrationServer
+
+from integration_server import NsqdIntegrationServer, LookupdIntegrationServer
+
+
+def test_basic():
+    with pytest.raises(ValueError):
+        Reader('test', 'test')
+
+    with pytest.raises(TypeError):
+        Reader(
+            topic='test',
+            channel='test',
+            nsqd_tcp_addresses=None,
+            lookupd_http_addresses='http://localhost:4161/',
+        )
+
+    with pytest.raises(TypeError):
+        Reader(
+            topic='test',
+            channel='test',
+            nsqd_tcp_addresses='localhost:4150',
+            lookupd_http_addresses=None,
+        )
+
+    def message_handler(reader, message):
+        pass
+
+    reader = Reader(
+        topic='test',
+        channel='test',
+        name='test',
+        max_concurrency=-1,
+        nsqd_tcp_addresses='localhost:4150',
+        lookupd_http_addresses='http://localhost:4161/',
+        message_handler=message_handler
+    )
+
+    assert reader.name == 'test'
+    assert reader.max_concurrency == multiprocessing.cpu_count()
+    assert len(reader.on_message.receivers) == 1
+
+    assert isinstance(reader.nsqd_tcp_addresses, set)
+    assert len(reader.nsqd_tcp_addresses) == 1
+
+    assert isinstance(reader.lookupds, list)
+    assert len(reader.lookupds) == 1
 
 
 @pytest.mark.slow
@@ -42,6 +92,7 @@ def test_messages():
 
             Accounting.count += 1
             if Accounting.count == Accounting.total:
+                assert not reader.is_starved
                 reader.close()
 
         try:
@@ -53,3 +104,152 @@ def test_messages():
             raise Accounting.error
 
         assert Accounting.count == Accounting.total
+
+
+@pytest.mark.slow
+def test_max_concurrency():
+    server1 = NsqdIntegrationServer()
+    server2 = NsqdIntegrationServer()
+
+    with server1, server2:
+        class Accounting(object):
+            count = 0
+            total = 100
+            concurrency = 0
+            error = None
+
+        for server in (server1, server2):
+            conn = Nsqd(
+                address=server.address,
+                tcp_port=server.tcp_port,
+                http_port=server.http_port,
+            )
+
+            for _ in xrange(Accounting.total / 2):
+                conn.publish_http('test', 'danger zone!')
+
+        reader = Reader(
+            topic='test',
+            channel='test',
+            nsqd_tcp_addresses=[
+                server1.tcp_address,
+                server2.tcp_address,
+            ],
+            max_in_flight=5,
+            max_concurrency=1,
+        )
+
+        @reader.on_exception.connect
+        def error_handler(reader, message, error):
+            if isinstance(error, NSQSocketError):
+                return
+            Accounting.error = error
+            reader.close()
+
+        @reader.on_message.connect
+        def handler(reader, message):
+            assert message.body == 'danger zone!'
+            assert Accounting.concurrency == 0
+
+            Accounting.concurrency += 1
+            gevent.sleep()
+            Accounting.concurrency -= 1
+
+            Accounting.count += 1
+            if Accounting.count == Accounting.total:
+                reader.close()
+
+        try:
+            reader.start()
+        except NSQSocketError:
+            pass
+
+        if Accounting.error:
+            raise Accounting.error
+
+        assert Accounting.count == Accounting.total
+
+
+@pytest.mark.slow
+def test_lookupd():
+    lookupd_server = LookupdIntegrationServer()
+    server1 = NsqdIntegrationServer(lookupd=lookupd_server.tcp_address)
+    server2 = NsqdIntegrationServer(lookupd=lookupd_server.tcp_address)
+
+    with lookupd_server, server1, server2:
+        class Accounting(object):
+            count = 0
+            total = 500
+            concurrency = 0
+            error = None
+
+        for server in (server1, server2):
+            conn = Nsqd(
+                address=server.address,
+                tcp_port=server.tcp_port,
+                http_port=server.http_port,
+            )
+
+            for _ in xrange(Accounting.total / 2):
+                conn.publish_http('test', 'danger zone!')
+
+        reader = Reader(
+            topic='test',
+            channel='test',
+            lookupd_http_addresses=lookupd_server.http_address,
+            max_in_flight=32,
+        )
+
+        @reader.on_exception.connect
+        def error_handler(reader, message, error):
+            if isinstance(error, NSQSocketError):
+                return
+            Accounting.error = error
+            reader.close()
+
+        @reader.on_message.connect
+        def handler(reader, message):
+            assert message.body == 'danger zone!'
+
+            Accounting.count += 1
+            if Accounting.count == Accounting.total:
+                reader.close()
+
+        try:
+            reader.start()
+        except NSQSocketError:
+            pass
+
+        if Accounting.error:
+            raise Accounting.error
+
+        assert Accounting.count == Accounting.total
+
+
+@pytest.mark.slow
+def test_backoff():
+    with NsqdIntegrationServer() as server:
+        conn = Nsqd(
+            address=server.address,
+            tcp_port=server.tcp_port,
+            http_port=server.http_port,
+        )
+
+        for _ in xrange(500):
+            conn.publish_http('test', 'danger zone!')
+
+        reader = Reader(
+            topic='test',
+            channel='test',
+            nsqd_tcp_addresses=[server.tcp_address],
+            max_in_flight=100,
+        )
+
+        reader.start(block=False)
+        reader.start_backoff()
+
+        assert reader.state == states.THROTTLED
+        assert reader.total_in_flight_or_ready <= 1
+
+        reader.complete_backoff()
+        assert reader.state == states.RUNNING
